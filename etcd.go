98d23df9c5341b2c9c0178f427ef01c957daffd9
lcbluestorm <lcbluestorm@gmail.com>
2016-08-08 12:24:46 +0800

    add etcd mutex lock

--- etcd.go
+++ etcd.go
@@ -1,47 +1,72 @@
 package lock
 
-
 import (
-    "time"
-    // "unicode"
+	"time"
 
-    "github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
+	// "unicode"
+
+	"github.com/coreos/etcd/client"
 )
-
 
 var ectdClient client.Client
 
-
-func registEtcdInstance(addrs []string, options map[string]string)  {
-    if ectdClient != nil{
-        return
-    }
-    transportType, ok := options["transport"]
-    transport := client.DefaultTransport
-    switch transportType {
-    case "type1":     
-    }
-    timeoutStr, ok := options["timeout"]
-    timeout := time.Second*30
-    if ok {
-        parsedTimeout, err := time.ParseDuration(timeoutStr)
-        if err == nil{
-            timeout = parsedTimeout
-        }else {
-            panic("the timeout value is err(should be 1s, 1us, 1ns, 1ms, 1m, 1h")
-        }
-    }
-    cfg := client.Config{
-        Endpoints: addrs, // []string{"http://127.0.0.1:2379"},
-        Transport: transport,
-        HeaderTimeoutPerRequest: timeout}
-    var err error = nil
-    ectdClient, err = client.New(cfg)
-    if err != nil{
-        panic(err)
-    }
+func registEtcdInstance(addrs []string, options map[string]string) {
+	if ectdClient != nil {
+		return
+	}
+	transportType, ok := options["transport"]
+	transport := client.DefaultTransport
+	switch transportType {
+	case "type1":
+	}
+	timeoutStr, ok := options["timeout"]
+	timeout := time.Second * 30
+	if ok {
+		parsedTimeout, err := time.ParseDuration(timeoutStr)
+		if err == nil {
+			timeout = parsedTimeout
+		} else {
+			panic("the timeout value is err(should be 1s, 1us, 1ns, 1ms, 1m, 1h")
+		}
+	}
+	cfg := client.Config{
+		Endpoints:               addrs, // []string{"http://127.0.0.1:2379"},
+		Transport:               transport,
+		HeaderTimeoutPerRequest: timeout}
+	var err error = nil
+	ectdClient, err = client.New(cfg)
+	if err != nil {
+		panic(err)
+	}
 }
 
 type EtcdMutexLock struct {
-    
+	Name    string
+	Expired int64
+}
+
+func (mutexLock EtcdMutexLock) Lock() error {
+	opts := &client.SetOptions{
+		PrevExist: client.PrevNoExist,
+		TTL:       time.Duration(mutexLock.Expired * 1000000)}
+	kapi := client.NewKeysAPI(ectdClient)
+	_, err := kapi.Set(context.TODO(), mutexLock.Name, mutexLock.Name, opts)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (mutexLock EtcdMutexLock) Release() error {
+	kapi := client.NewKeysAPI(ectdClient)
+	_, err := kapi.Delete(context.TODO(), mutexLock.Name, nil)
+	if err == nil {
+		return nil
+	}
+	e, ok := err.(client.Error)
+	if ok && e.Code == client.ErrorCodeKeyNotFound {
+		return nil
+	}
+	return err
 }
--- lock.go
+++ lock.go
@@ -1,44 +1,55 @@
 package lock
 
 import (
-    "math/rand"
-    "time"
+	"math/rand"
+	"time"
 )
 
-
 type Lock interface {
-    Lock() error
-    Release() error
+	Lock() error
+	Release() error
 }
 
-
-func RegisterLock(lockType string, addrs []string, options map[string]string)  {
-    if lockType == "redis" {
-        registRedisInstance(addrs, options)
-    }else if lockType == "etcd"{
-        registEtcdInstance(addrs, options)
-    }
+func RegisterLock(lockType string, addrs []string, options map[string]string) {
+	if lockType == "redis" {
+		registRedisInstance(addrs, options)
+	} else if lockType == "etcd" {
+		registEtcdInstance(addrs, options)
+	}
 }
-
 
 func GetDefaultRedisMutexLock() *RedisMutexLock {
-    if len(redisConns) == 0{
-        panic("You must register redis instance first")
-    }
-    rand.Seed(time.Now().Unix())
-    ramdomNum := rand.Int63()
-    return &RedisMutexLock{Name:"defaultRedisLock", Expired:3000, randomKey:ramdomNum}
+	if len(redisConns) == 0 {
+		panic("You must register redis instance first")
+	}
+	rand.Seed(time.Now().Unix())
+	ramdomNum := rand.Int63()
+	return &RedisMutexLock{Name: "defaultRedisMutexLock", Expired: 3000, randomKey: ramdomNum}
 }
 
 func GetRedisMutexLock(name string, expired int64) *RedisMutexLock {
-    if len(redisConns) == 0{
-        panic("You must register redis instance first")
-    }
-    rand.Seed(time.Now().Unix())
-    ramdomNum := rand.Int63()
-    return &RedisMutexLock{Name:name, Expired:expired, randomKey:ramdomNum}
+	if len(redisConns) == 0 {
+		panic("You must register redis instance first")
+	}
+	rand.Seed(time.Now().Unix())
+	ramdomNum := rand.Int63()
+	return &RedisMutexLock{Name: name, Expired: expired, randomKey: ramdomNum}
 }
 
-func GetDefaultRedisRWLock()  {
-    
+// func GetDefaultRedisRWLock()  {
+
+// }
+
+func GetDefaultEtcdMutexLock() *EtcdMutexLock {
+	if ectdClient == nil {
+		panic("You must register etct instance first")
+	}
+	return &EtcdMutexLock{Name: "defaultEtcdMutexLock", Expired: 3000}
+}
+
+func GetEtcdMutexLock(name string, expired int64) *EtcdMutexLock {
+	if ectdClient == nil {
+		panic("You must register etct instance first")
+	}
+	return &EtcdMutexLock{Name: name, Expired: expired}
 }
