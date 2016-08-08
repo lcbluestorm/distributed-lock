d9d09f22c0427ffbab035506ce9b6275ff3b922b
lcbluestorm <lcbluestorm@gmail.com>
2016-08-08 13:03:04 +0800

    update test case

--- etcd_test.go
+++ etcd_test.go
@@ -1,24 +1,23 @@
 package lock
 
 import (
-    "testing"
-    "fmt"
+	"fmt"
+	"testing"
 )
 
-func TestEtcd(t *testing.T)  {
-    fmt.Println("etcd mutex lock test start")
-    config := []string{":18000", ":18001", ":18002"}
-    var options map[string]string
-    RegisterLock("etcd", config, options)
-    // lock := GetDefaultRedisLock()
-    lock := GetRedisMutexLock("test", 10000)
-    fmt.Println(lock)
-    err := lock.Lock()
-    if err != nil{
-        fmt.Println("get lock failed")
-    }
-    
-    fmt.Println("redis mutex locktest end")
+func TestEtcd(t *testing.T) {
+	fmt.Println("etcd mutex lock test start")
+	config := []string{":18000", ":18001", ":18002"}
+	var options map[string]string
+	RegisterLock("etcd", config, options)
+	// lock := GetDefaultRedisLock()
+	lock := GetEtcdMutexLock("test", 10000)
+	fmt.Println(lock)
+	err := lock.Lock()
+	if err != nil {
+		fmt.Println("get lock failed")
+	}
+
+	fmt.Println("redis mutex locktest end")
 
 }
-
